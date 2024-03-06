d7b3c74403bd0db6bb1513d12ccad2b3ea543626
Ariba Nadir <50998843+Aribanadir@users.noreply.github.com>
2024-03-06 21:14:11 +0100

    Update like_product.php

--- like_product.php
+++ like_product.php
@@ -1,5 +1,5 @@
 <?php
-// Vérifier si l'ID du produit est fourni dans la requête boutique
+// Vérifier si l'ID du produit est fourni dans 
 if (isset($_POST['user_id'])) {
     // Récupérer l'ID du produit depuis la requête POST
     $productId = $_POST['user_id'];
